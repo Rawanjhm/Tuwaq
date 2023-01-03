2964c18e919af258e83d92aecd24cd00c78826f6
Rawanjhm <121760126+Rawanjhm@users.noreply.github.com>
2023-01-03 16:14:58 +0300

    Add files via upload

---
+++ op.kt
@@ -0,0 +1,22 @@
+package com.example.myprogramming
+
+fun main(){
+        false && false
+        /*false*/
+         10 % 3
+      /* 3.3333*/
+    5 >= 5
+    /* 5*/
+    false || true
+    /* true */
+    5-3*2
+    /* 1 */
+    3!=2
+    /* yes */
+    (6+6)/3
+    /* 4*?
+
+
+
+
+}
