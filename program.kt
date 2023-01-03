1bbef543d91f5163e9565c79c231914013312ff6
Rawanjhm <121760126+Rawanjhm@users.noreply.github.com>
2023-01-03 15:23:07 +0300

    Add files via upload

---
+++ program.kt
@@ -0,0 +1,21 @@
+package com.example.myprogramming
+
+fun main(){
+
+           for (i in 1..100){
+               if (i <= 70 )
+               println(i)
+               else {
+                   println("it's over 70")
+                   break
+               }
+
+
+    }
+
+
+
+
+}
+
+
