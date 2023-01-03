e7e59cf6c65ad2f879fb327aae261f9f5938c49f
Rawanjhm <121760126+Rawanjhm@users.noreply.github.com>
2023-01-03 15:09:59 +0300

    Add files via upload

---
+++ ex2.kt
@@ -0,0 +1,15 @@
+package com.example.myprogramming
+
+ fun main() {
+
+              var x =35
+     while (x == 35) {
+         x -= 5
+         println("$x it's hot  ")
+         x -= 10
+         println("$x it is comfy ")
+         x -= 5
+         println("$x it is cold ")
+     }
+ }
+
